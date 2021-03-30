a1b4cdfd056befe6c471aaa8cdb765e19acab250
Felipe Rosa Amorim <framorim1987@gmail.com>
2021-03-30 03:19:31 -0300

    add finishTask and deleteTask

--- JS na Web Manipule o DOM com JavaScript/script.js
+++ JS na Web Manipule o DOM com JavaScript/script.js
@@ -1,20 +1,53 @@
-const btnAdd = document.querySelector('.form-button')
+(() => {
+    const btnAdd = document.querySelector('.form-button')
 
-const createTask = event => {
-    event.preventDefault()
-    
-    const input = document.querySelector('.form-input')
-    const taskList = document.querySelector('.list')
-    
-    const task = document.createElement('li')
-    task.classList.add('task')
+    const createTask = event => {
 
-    const content = `<p class='content'>${input.value}</p>`
+        event.preventDefault()
 
-    task.innerHTML = content
-    taskList.appendChild(task)
+        const input = document.querySelector('.form-input')
+        const taskList = document.querySelector('.list')
 
-    input.value = ''
-}
+        const task = document.createElement('li')
+        task.classList.add('task')
 
-btnAdd.addEventListener('click', createTask)
+        const content = `<p class='content'>${input.value}</p>`
+
+        task.innerHTML = content
+        task.appendChild(BtnFinish())
+        task.appendChild(BtnDelete())
+        taskList.appendChild(task)
+
+        input.value = ''
+    }
+
+    btnAdd.addEventListener('click', createTask)
+
+    const BtnFinish = () => {
+
+        const btnFinish = document.createElement('button')
+        btnFinish.innerText = 'Done'
+        btnFinish.addEventListener('click', finishTask)
+        
+        return btnFinish
+    }
+
+    const finishTask = event => {
+
+        event.target.parentElement.classList.toggle('done')
+    }
+
+    const BtnDelete = () => {
+
+        const btnDelete = document.createElement('button')
+        btnDelete.innerText = 'Delete'
+        btnDelete.addEventListener('click', deleteTask)
+        
+        return btnDelete
+    }
+
+    const deleteTask = event => {
+
+        event.target.parentElement.remove()
+    }
+})()
